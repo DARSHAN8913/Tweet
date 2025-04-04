84cfca0f5946d5dd18ec8bfd53f7b5cd9c3cc8b4
DARSHAN8913 <ddshatagar@gmail.com>
2025-04-04 13:27:29 +0530

    1327

--- twitter_clone/users/admin.py
+++ twitter_clone/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    fieldsets = DefaultUserAdmin.fieldsets + (
+        ('Profile Info', {'fields': ('bio', 'website', 'profile_image')}),
+    )
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'follower_count']
--- twitter_clone/users/models.py
+++ twitter_clone/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser to include additional profile information
+    """
+    bio = models.TextField(max_length=160, blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    website = models.URLField(blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    joined_date = models.DateTimeField(auto_now_add=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+    @property
+    def follower_count(self):
+        return self.followers.count()
+    
+    @property
+    def following_count(self):
+        return self.following.count()
